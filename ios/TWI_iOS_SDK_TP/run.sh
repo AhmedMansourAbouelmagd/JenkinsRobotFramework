9817307df1da4f83e4539f50744db4eb1faa6363
khaledElgazzzar <113449056+khaledElgazzzar@users.noreply.github.com>
2022-11-01 17:40:06 +0200

    aa

--- ios/TWI_iOS_SDK_TP/run.sh
+++ ios/TWI_iOS_SDK_TP/run.sh
@@ -1,2 +1,2 @@
-cd /Users/user/Jenkins/workspace/Insulet_IOS_SDK_TP/ios/TWI_iOS_SDK_TP
+cd /Users/user/Jenkins/workspace/Insulet_IOS_SDK_TP/ios/TWI_iOS_SDK_TP/Tests
 robot -d results System_Comptability_Test_Cases.robot
